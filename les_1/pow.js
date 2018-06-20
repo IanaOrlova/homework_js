55876e07f090440cc932a2f871b4d677b35ee78d
Yana <38989612+IanaOrlova@users.noreply.github.com>
2018-06-20 23:43:03 +0300

    js

---
+++ les_1/pow.js
@@ -0,0 +1,20 @@
+alert("Я люблю БОРЩ");
+
+var name;
+var admin;
+
+name = "Василий";
+admin = name;
+
+alert(admin);
+
+
+var our_planet = "Земля";
+var username = "Петр";
+
+
+var userName = prompt("Как Ваше имя?");
+ var planen = prompt("С какой Вы с планеты ?");
+
+
+alert("Как насчет борща "+userName+" с планеты "+planen+ "?");
