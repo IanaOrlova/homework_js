53a2a9db746d014015e826e67572bada33f2e1b4
Yana <38989612+IanaOrlova@users.noreply.github.com>
2018-08-01 16:14:50 +0300

    slick slider

---
+++ les_9/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.container').slick({
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            prevArrow:'<img  class="prev" src="img/Layer_left.png">',
+            nextArrow:'<img class="next" src="img/Layer_right.png">',
+            variableWidth: true,
+        });
+});
